70ab49351ddf85b2d9a4c2ff77c4c0f4f129c47e
goldman2g5 <95956749+goldman2g5@users.noreply.github.com>
2021-12-16 21:21:13 +0300

    Бэбра

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URl"] = "sqlite:////debil.db"
-db = SQLAlchemy(app)
-
-
-class Article(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    intro = db.Column(db.String, nullable=False)
-    text = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return "<Article %r" % self.id
 
 
 @app.route('/login/', methods=['post', 'get'])
@@ -27,8 +14,7 @@ def login():
         message = "Correct username and password"
     else:
         message = "Wrong username or password"
-
-    return render_template('ауе.html', message=message)
+    return render_template('index.html', message=message)
 
 
 @app.route('/user/<string:name>/<int:id>')
@@ -39,4 +25,3 @@ def user(name, id):
 if __name__ == "__main__":
     app.run(debug=True)
 
-db.create_all()
