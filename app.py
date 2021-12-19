8a5c273c7b642e178657e9de55a1db94a3a716a3
goldman2g5 <95956749+goldman2g5@users.noreply.github.com>
2021-12-19 04:42:56 +0300

    дурка2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import pymysql
 from pymysql.cursors import DictCursor
 
@@ -22,25 +22,14 @@ def login():
     a = cur.execute(f'SELECT login, password FROM users WHERE login = "{username}" AND password = "{password}";')
 
     if a == 1:
-        message = "Correct username and paassword"
+        return redirect("/neyronka")
     elif a == 0:
         message = "Wrong username or password"
     return render_template('login.html', message=message)
-
 
 @app.route('/neyronka/', methods=['post', 'get'])
-def neyronka():
-    message = ''
-    username = request.form.get('username')  # запрос к данным формы
-    password = request.form.get('password')
-    cur = dbh.cursor()
-    a = cur.execute(f'SELECT login, password FROM users WHERE login = "{username}" AND password = "{password}";')
-
-    if a == 1:
-        message = "Correct username and password"
-    elif a == 0:
-        message = "Wrong username or password"
-    return render_template('login.html', message=message)
+def huy():
+    return render_template('neyronka.html')
 
 
 @app.route('/user/<string:name>/<int:id>')
