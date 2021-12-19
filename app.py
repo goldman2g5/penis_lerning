3c916f6a8345c182ba730e89a1b24e5c8767193b
Sharp336 <maxsh2005@gmail.com>
2021-12-19 16:34:38 +0300

    логин и регистрация с бд и редиректами

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 import pymysql
 from pymysql.cursors import DictCursor
 
@@ -15,17 +15,44 @@ dbh = pymysql.connect(
 
 @app.route('/login/', methods=['post', 'get'])
 def login():
-    message = ''
-    username = request.form.get('username')  # запрос к данным формы
+    message = 'Для входа введите логин и пароль'
+    login = request.form.get('username')  # запрос к данным формы
     password = request.form.get('password')
-    cur = dbh.cursor()
-    a = cur.execute(f'SELECT login, password FROM users WHERE login = "{username}" AND password = "{password}";')
-
-    if a == 1:
-        return redirect("/neyronka")
-    elif a == 0:
-        message = "Wrong username or password"
+    if login != None or password != None:
+        cur = dbh.cursor()
+        cur.execute(f'SELECT login, password FROM users WHERE login = "{login}" AND password = "{password}";')
+        a = cur.fetchone()
+        if a:
+            if a['login'] == login and a['password'] == password:
+                return redirect("/neyronka")
+        else:
+            message = "Wrong username or password"
     return render_template('login.html', message=message)
+
+@app.route('/reg/', methods=['post', 'get'])
+def registration():
+    message = 'Для регистрации заполните форму ниже'
+
+    login = request.form.get('login')  # запрос к данным формы
+    password = request.form.get('password')
+    username = request.form.get('username')
+    print(f'l - {login},p - {password},u - {username}')
+    print(f'l - {type(login)},p - {type(password)},u - {type(username)}')
+    if login and password and username and len(login) >= 4 and len(password) >= 4 and len(username) >= 4:
+        cur = dbh.cursor()
+        cur.execute(f'SELECT login, password FROM users WHERE login = "{login}" OR password = "{password}" OR name = "{username}";')
+        b = cur.fetchone()
+        print(f'b = {b}')
+        if b == None:
+            cur.execute(f'INSERT INTO users (id, login, password, name) VALUES (NULL, "{login}", "{password}", "{username}");')
+            dbh.commit()
+            message = "Успешная регистрация"
+            return redirect("/login")
+        else:
+            message = "Логин, пароль или имя уже используются, попробуйте другие"
+
+    return render_template('reg.html', message = message)
+
 
 @app.route('/neyronka/', methods=['post', 'get'])
 def huy():
