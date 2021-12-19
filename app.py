632b65883660c3b9fb869291ac3926fbbf3d5466
goldman2g5 <95956749+goldman2g5@users.noreply.github.com>
2021-12-19 03:44:16 +0300

    дурка

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 from flask import Flask, render_template, request
+import pymysql
+from pymysql.cursors import DictCursor
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URl"] = "sqlite:////debil.db"
 
+dbh = pymysql.connect(
+        host='185.12.94.106',
+        user='2p1s23',
+        password='941-961-748',
+        db='2p1s23',
+        charset='utf8mb4',
+        cursorclass = DictCursor
+    )
 
 @app.route('/login/', methods=['post', 'get'])
 def login():
     message = ''
     username = request.form.get('username')  # запрос к данным формы
     password = request.form.get('password')
+    cur = dbh.cursor()
+    a = cur.execute(f'SELECT login, password FROM users WHERE login = "{username}" AND password = "{password}";')
 
-    if username == 'root' and password == 'pass':
+    if a == 1:
         message = "Correct username and password"
-    else:
+    elif a == 0:
         message = "Wrong username or password"
     return render_template('index.html', message=message)
 
