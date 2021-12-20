448d2e92e9ec3b01ec60b5c9b18faca3f080393b
denamo <artstarxd@outlook.com>
2021-12-20 15:15:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,27 +19,33 @@ dbh = pymysql.connect(
 def login():
     message = 'Для входа введите логин и пароль'
     login, password = request.form.get('username'), request.form.get('password')  # запрос к данным формы
-    if login != None or password != None:
+    if login is not None or password is not None:
         cur = dbh.cursor()
         cur.execute(f'SELECT login, password FROM users WHERE login = "{login}" AND password = "{password}";')
         a = cur.fetchone()
-        if a and a['login'] == login and a['password'] == password: return redirect("/neyronka")
-        else: message = "Wrong username or password"
+        if a and a['login'] == login and a['password'] == password:
+            return redirect("/neyronka")
+        else:
+            message = "Wrong username or password"
     return render_template('login.html', message=message)
 
 
 @app.route('/reg/', methods=['post', 'get'])
 def registration():
     message = 'Для регистрации заполните форму ниже'
-    login, password, username = request.form.get('login'), request.form.get('password'), request.form.get('username')  # запрос к данным формы
+    login, password, username = request.form.get('login'), request.form.get('password'), request.form.get(
+        'username')  # запрос к данным формы
     if login and password and username and len(login) >= 4 and len(password) >= 4 and len(username) >= 4:
         cur = dbh.cursor()
-        cur.execute(f'SELECT login, password FROM users WHERE login = "{login}" OR password = "{password}" OR name = "{username}";')
+        cur.execute(
+            f'SELECT login, password FROM users WHERE login = "{login}" OR password = "{password}" OR name = "{username}";')
         b = cur.fetchone()
-        if b == None:
-            cur.execute(f'INSERT INTO users (id, login, password, name) VALUES (NULL, "{login}", "{password}", "{username}");')
+        if b is None:
+            cur.execute(
+                f'INSERT INTO users (id, login, password, name) VALUES (NULL, "{login}", "{password}", "{username}");')
             return redirect("/login")
-        else: message = "Логин, пароль или имя уже используются, попробуйте другие"
+        else:
+            message = "Логин, пароль или имя уже используются, попробуйте другие"
 
     return render_template('reg.html', message=message)
 
