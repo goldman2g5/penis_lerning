bc1dc89e33ba4eab54a1d6dbd5e80ef42b11ad6b
m0rp3x <67301219+m0rp3x@users.noreply.github.com>
2021-12-21 17:39:22 +0300

    анимированный фон и добавление js

---
+++ js/gpaphic.js
@@ -0,0 +1,10 @@
+VANTA.RINGS({
+    el: "#intro",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00
+})
