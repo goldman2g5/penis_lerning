a0efe294ddaed0053365b8dca2ffd91080bfcd81
m0rp3x <yurash612@gmail.com>
2021-12-26 17:14:01 +0300

    анавальный бегает по полю

--- static/js/neyronka.js
+++ static/js/neyronka.js
@@ -1,187 +1,241 @@
-var canvas = document.getElementById("canvas");
+console.clear()
+console.log('lsakdfalskjdflnksd')
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-// Initialize the GL context
-var gl = canvas.getContext('webgl');
-if(!gl){
-    console.error("Unable to initialize WebGL.");
+const config = {
+    src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/175711/open-peeps-sheet.png',
+    rows: 15,
+    cols: 7
 }
 
-//Time
-var time = 0.0;
+// UTILS
 
-//************** Shader sources **************
+const randomRange = (min, max) => min + Math.random() * (max - min)
 
-var vertexSource = `
-attribute vec2 position;
-void main() {
-  gl_Position = vec4(position, 0.0, 1.0);
-}
-`;
+const randomIndex = (array) => randomRange(0, array.length) | 0
 
-var fragmentSource = `
-precision highp float;
+const removeFromArray = (array, i) => array.splice(i, 1)[0]
 
-#define AA
+const removeItemFromArray = (array, item) => removeFromArray(array, array.indexOf(item))
 
-uniform float width;
-uniform float height;
-vec2 resolution = vec2(width, height);
+const removeRandomFromArray = (array) => removeFromArray(array, randomIndex(array))
 
-uniform float time;
+const getRandomFromArray = (array) => (
+    array[randomIndex(array) | 0]
+)
 
-void main(){
+// TWEEN FACTORIES
 
-	float strength = 0.4;
-	float t = time/6.0;
+const resetPeep = ({ stage, peep }) => {
+    const direction = Math.random() > 0.5 ? 1 : -1
+    // using an ease function to skew random to lower values to help hide that peeps have no legs
+    const offsetY = 100 - 250 * gsap.parseEase('power2.in')(Math.random())
+    const startY = stage.height - peep.height + offsetY
+    let startX
+    let endX
 
-	vec3 col = vec3(0);
-	vec2 fC = gl_FragCoord.xy;
-
-	#ifdef AA
-	for(int i = -1; i <= 1; i++) {
-		for(int j = -1; j <= 1; j++) {
-
-			fC = gl_FragCoord.xy+vec2(i,j)/3.0;
-
-			#endif
-
-			//Normalized pixel coordinates (from 0 to 1)
-			vec2 pos = fC/resolution.xy;
-
-			pos.y /= resolution.x/resolution.y;
-			pos = 4.0*(vec2(0.5) - pos);
-
-			for(float k = 1.0; k < 7.0; k+=1.0){ 
-				pos.x += strength * sin(2.0*t+k*1.5 * pos.y)+t*0.5;
-				pos.y += strength * cos(2.0*t+k*1.5 * pos.x);
-			}
-
-			//Time varying pixel colour
-			col += 0.5 + 0.5*cos(time+pos.xyx+vec3(0,2,4));
-
-			#ifdef AA
-		}
-	}
-
-	col /= 9.0;
-	#endif
-
-  //Gamma
-  col = pow(col, vec3(0.4545));
-
-	//Fragment colour
-	gl_FragColor = vec4(col,1.0);
-}
-`;
-
-//************** Utility functions **************
-
-window.addEventListener( 'resize', onWindowResize, false );
-
-function onWindowResize(){
-    canvas.width  = window.innerWidth;
-    canvas.height = window.innerHeight;
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.uniform1f(widthHandle, window.innerWidth);
-    gl.uniform1f(heightHandle, window.innerHeight);
-}
-
-
-//Compile shader and combine with source
-function compileShader(shaderSource, shaderType){
-    var shader = gl.createShader(shaderType);
-    gl.shaderSource(shader, shaderSource);
-    gl.compileShader(shader);
-    if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
-        throw "Shader compile failed with: " + gl.getShaderInfoLog(shader);
+    if (direction === 1) {
+        startX = -peep.width
+        endX = stage.width
+        peep.scaleX = 1
+    } else {
+        startX = stage.width + peep.width
+        endX = 0
+        peep.scaleX = -1
     }
-    return shader;
-}
 
-//From https://codepen.io/jlfwong/pen/GqmroZ
-//Utility to complain loudly if we fail to find the attribute/uniform
-function getAttribLocation(program, name) {
-    var attributeLocation = gl.getAttribLocation(program, name);
-    if (attributeLocation === -1) {
-        throw 'Cannot find attribute ' + name + '.';
+    peep.x = startX
+    peep.y = startY
+    peep.anchorY = startY
+
+    return {
+        startX,
+        startY,
+        endX
     }
-    return attributeLocation;
 }
 
-function getUniformLocation(program, name) {
-    var attributeLocation = gl.getUniformLocation(program, name);
-    if (attributeLocation === -1) {
-        throw 'Cannot find uniform ' + name + '.';
+const normalWalk = ({ peep, props }) => {
+    const {
+        startX,
+        startY,
+        endX
+    } = props
+
+    const xDuration = 10
+    const yDuration = 0.25
+
+    const tl = gsap.timeline()
+    tl.timeScale(randomRange(0.5, 1.5))
+    tl.to(peep, {
+        duration: xDuration,
+        x: endX,
+        ease: 'none'
+    }, 0)
+    tl.to(peep, {
+        duration: yDuration,
+        repeat: xDuration / yDuration,
+        yoyo: true,
+        y: startY - 10
+    }, 0)
+
+    return tl
+}
+
+const walks = [
+    normalWalk,
+]
+
+// CLASSES
+
+class Peep {
+    constructor({
+                    image,
+                    rect,
+                }) {
+        this.image = image
+        this.setRect(rect)
+
+        this.x = 0
+        this.y = 0
+        this.anchorY = 0
+        this.scaleX = 1
+        this.walk = null
     }
-    return attributeLocation;
+
+    setRect (rect) {
+        this.rect = rect
+        this.width = rect[2]
+        this.height = rect[3]
+
+        this.drawArgs = [
+            this.image,
+            ...rect,
+            0, 0, this.width, this.height
+        ]
+    }
+
+    render (ctx) {
+        ctx.save()
+        ctx.translate(this.x, this.y)
+        ctx.scale(this.scaleX, 1)
+        ctx.drawImage(...this.drawArgs)
+        ctx.restore()
+    }
 }
 
-//************** Create shaders **************
+// MAIN
 
-//Create vertex and fragment shaders
-var vertexShader = compileShader(vertexSource, gl.VERTEX_SHADER);
-var fragmentShader = compileShader(fragmentSource, gl.FRAGMENT_SHADER);
+const img = document.createElement('img')
+img.onload = init
+img.src = config.src
 
-//Create shader programs
-var program = gl.createProgram();
-gl.attachShader(program, vertexShader);
-gl.attachShader(program, fragmentShader);
-gl.linkProgram(program);
+const canvas = document.querySelector('#canvas')
+const ctx = canvas.getContext('2d')
 
-gl.useProgram(program);
-
-//Set up rectangle covering entire canvas
-var vertexData = new Float32Array([
-    -1.0,  1.0, 	// top left
-    -1.0, -1.0, 	// bottom left
-    1.0,  1.0, 	// top right
-    1.0, -1.0, 	// bottom right
-]);
-
-//Create vertex buffer
-var vertexDataBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, vertexDataBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
-
-// Layout of our data in the vertex buffer
-var positionHandle = getAttribLocation(program, 'position');
-
-gl.enableVertexAttribArray(positionHandle);
-gl.vertexAttribPointer(positionHandle,
-    2, 				// position is a vec2 (2 values per component)
-    gl.FLOAT, // each component is a float
-    false, 		// don't normalize values
-    2 * 4, 		// two 4 byte float components per vertex (32 bit float is 4 bytes)
-    0 				// how many bytes inside the buffer to start from
-);
-
-//Set uniform handle
-var timeHandle = getUniformLocation(program, 'time');
-var widthHandle = getUniformLocation(program, 'width');
-var heightHandle = getUniformLocation(program, 'height');
-
-gl.uniform1f(widthHandle, window.innerWidth);
-gl.uniform1f(heightHandle, window.innerHeight);
-
-var lastFrame = Date.now();
-var thisFrame;
-
-function draw(){
-
-    //Update time
-    thisFrame = Date.now();
-    time += (thisFrame - lastFrame)/770;
-    lastFrame = thisFrame;
-
-    //Send uniforms to program
-    gl.uniform1f(timeHandle, time);
-    //Draw a triangle strip connecting vertices 0-4
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-
-    requestAnimationFrame(draw);
+const stage = {
+    width: 0,
+    height: 0,
 }
 
-draw();
+const allPeeps = []
+const availablePeeps = []
+const crowd = []
+
+function init () {
+    createPeeps()
+
+    // resize also (re)populates the stage
+    resize()
+
+    gsap.ticker.add(render)
+    window.addEventListener('resize', resize)
+}
+
+function createPeeps () {
+    const {
+        rows,
+        cols
+    } = config
+    const {
+        naturalWidth: width,
+        naturalHeight: height
+    } = img
+    const total = rows * cols
+    const rectWidth = width / rows
+    const rectHeight = height / cols
+
+    for (let i = 0; i < total; i++) {
+        allPeeps.push(new Peep({
+            image: img,
+            rect: [
+                (i % rows) * rectWidth,
+                (i / rows | 0) * rectHeight,
+                rectWidth,
+                rectHeight,
+            ]
+        }))
+    }
+}
+
+function resize () {
+    stage.width = canvas.clientWidth
+    stage.height = canvas.clientHeight
+    canvas.width = stage.width * devicePixelRatio
+    canvas.height = stage.height * devicePixelRatio
+
+    crowd.forEach((peep) => {
+        peep.walk.kill()
+    })
+
+    crowd.length = 0
+    availablePeeps.length = 0
+    availablePeeps.push(...allPeeps)
+
+    initCrowd()
+}
+
+function initCrowd () {
+    while (availablePeeps.length) {
+        // setting random tween progress spreads the peeps out
+        addPeepToCrowd().walk.progress(Math.random())
+    }
+}
+
+function addPeepToCrowd () {
+    const peep = removeRandomFromArray(availablePeeps)
+    const walk = getRandomFromArray(walks)({
+        peep,
+        props: resetPeep({
+            peep,
+            stage,
+        })
+    }).eventCallback('onComplete', () => {
+        removePeepFromCrowd(peep)
+        addPeepToCrowd()
+    })
+
+    peep.walk = walk
+
+    crowd.push(peep)
+    crowd.sort((a, b) => a.anchorY - b.anchorY)
+
+    return peep
+}
+
+function removePeepFromCrowd (peep) {
+    removeItemFromArray(crowd, peep)
+    availablePeeps.push(peep)
+}
+
+function render () {
+    canvas.width = canvas.width
+    ctx.save()
+    ctx.scale(devicePixelRatio, devicePixelRatio)
+
+    crowd.forEach((peep) => {
+        peep.render(ctx)
+    })
+
+    ctx.restore()
+}
+
