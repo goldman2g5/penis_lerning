d8e1c0f3d09d185496c68bf71e3d65caf33adaac
goldman2g5 <tima.radchenko.84@mail.ru>
2021-12-26 15:11:09 +0300

    Когда воскрес  аллах, я срал на горе абдулахамедадиуйди

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 
 import pymysql
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect
 from pymysql.cursors import DictCursor
 
 app = Flask(__name__)
@@ -60,30 +60,51 @@ def login():
             return redirect("/neyronka")
         else:
             message = "Wrong username or password"
-    return render_template('login.html')
+    return render_template('login.html', message=message)
 
 @app.route('/neyronka', methods=['post', 'get'])
 def neyronka():
     return render_template('neyronka.html')
 
+
+keys_list = []
+value_list = []
 @app.route('/get_piskka', methods=['GET', 'POST'])
 def get_name():
     name = request.form['bd_type']
-    print(name)
-    return render_template('login.html')
+    if name:
+        cur = dbh.cursor()
+        cur.execute(f"SELECT *FROM {name}")
+        var = cur.fetchone()
+        print(var)
+        global keys_list
+        global value_list
+
+        value_list = list(var.values())
+        keys_list = list(var.keys())
+
+
+    return render_template('baza.html')
+
 
 @app.route('/gigabaza', methods=['post', 'get'])
 def baza():
-    cur = dbh.cursor()
-    table = request.form.get('admins')
-    """print(table)"""
-    # cur.execute(f"SELECT admins.id, admins.name, room.id, room.room_name FROM admins, room, room_rent, console, guests")
-    cur.execute(f"SELECT *FROM admins, room")
-    var = cur.fetchone()  # room.console_id, room.admin_id, room_rent.id, room_rent.rent_time, room_rent.guest_id, room_rent.room_id
-    print(var)
-    admins, room = var.get("admins"), var.get("room")
-    return render_template('baza.html', table=table, admin_name=room)
+    var1 = keys_list[0] if len(keys_list) >= 1 else ""
+    var2 = keys_list[1] if len(keys_list) >= 2 else ""
+    var3 = keys_list[2] if len(keys_list) >= 3 else ""
+    var4 = keys_list[3] if len(keys_list) >= 4 else ""
+    var5 = keys_list[4] if len(keys_list) >= 5 else ""
+    val1 = value_list[0] if len(value_list) >= 1 else ""
+    val2 = value_list[1] if len(value_list) >= 2 else ""
+    val3 = value_list[2] if len(value_list) >= 3 else ""
+    val4 = value_list[3] if len(value_list) >= 4 else ""
+    val5 = value_list[4] if len(value_list) >= 5 else ""
+
+    return render_template('baza.html', keys_list=keys_list, value_list=value_list)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# cur.execute(f"SELECT admins.id, admins.name, room.id, room.room_name FROM admins, room, room_rent, console, guests")
+# room.console_id, room.admin_id, room_rent.id, room_rent.rent_time, room_rent.guest_id, room_rent.room_id
