00bf6d9f11b6fd54721ffd94ab2daee3d68c13f0
goldman2g5 <tima.radchenko.84@mail.ru>
2021-12-27 19:10:58 +0300

    Бегу на защита проекта со всех ножек стула запиханных в диван-тыгыдык тыгыдык

---
+++ static/js/basa.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+const select = document.querySelector('#zalupa').getElementsByTagName('option');
+var urlsub = window.location.href.split('?')[1];
+for (let i = 0; i < select.length; i++) {
+    if (select[i].value === urlsub) select[i].selected = true;
+}
+});
+$(document).ready(function () {
+  $("#form").submit(function (event) {
+    var zalupa = $('#zalupa').val();
+    var url = window.location.href.split('?')[0];
+    var url = url + '?' + zalupa;
+    $.ajax({
+      type: "POST",
+      url: "/get_table",
+      data: {
+        zalupa:zalupa,
+      },
+      success: function () {
+        location.assign(url);
+
+      }
+    });
+    return false; //<---- move it here
+  });}
+);
+
+
+$(document).ready(function () {
+  $("#form_input").submit(function (event) {
+    var name = $('#get_name').val();
+    var url = location.href;
+    var update_table = url.substring(url.indexOf("?")+1);
+    $.ajax({
+      type: "POST",
+      url: "/get_input",
+      data: {
+        name:name,
+        update_table:update_table,
+      },
+
+      success: function () {
+        location.reload();
+      }
+    });
+    return false; //<---- move it here
+  });}
+);
+
+$(document).ready(function () {
+  $("#form_delete").submit(function (event) {
+    var users_to_delete = $('#users_to_delete').val();
+    var url = location.href;
+    var update_table = url.substring(url.indexOf("?")+1);
+    $.ajax({
+      type: "POST",
+      url: "/delete_user",
+      data: {
+        users_to_delete:users_to_delete,
+        update_table:update_table,
+      },
+
+      success: function () {
+        location.reload();
+      }
+    });
+    return false; //<---- move it here
+  });}
+);
